6100b9e0dc10921b39e1276ef27fd93b8b4aaf49
Adam Kiss <iam@adamkiss.com>
2022-12-01 16:36:01 +0100

    Day 01

--- 01.js
+++ 01.js
@@ -1,9 +1,21 @@
 const {readFileSync} = require('fs')
+const {ray} = require('node-ray')
 
-const demo1 = readFileSync('./inputs/01-1-demo.txt')
-const real1 = readFileSync('./inputs/01-1.txt')
+const demo1 = readFileSync('./inputs/01-1-demo.txt', 'utf-8')
+const real1 = readFileSync('./inputs/01-1-real.txt', 'utf-8')
+
+const elves = input => input.split("\n\n").map(elf => elf.split("\n").reduce((p, c) => p + Number(c), 0))
 
 const r01 = input => {
-    const elves = input.split("\n\n").map(elf => elf.split("\n").reduce())
+    ray(Math.max(...elves(input)))
 }
+
+const r02 = input => {
+    const sorted = elves(input).sort((a, b) => b-a)
+    ray(sorted.shift() + sorted.shift() + sorted.shift())
+}
+
+r01(demo1)
+r01(real1)
+r02(real1)
 
