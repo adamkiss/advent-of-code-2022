e050c59ae11378febf8fbf0afb2da387a5670312
Adam Kiss <iam@adamkiss.com>
2022-12-02 17:07:08 +0100

    DAY2: HAHAHA BIG BRAIN TIME

--- 02.js
+++ 02.js
@@ -3,7 +3,6 @@ const {ray} = require('node-ray')
 
 const demo1 = readFileSync('./inputs/02-1-demo.txt', 'utf-8')
 const real1 = readFileSync('./inputs/02-1-real.txt', 'utf-8')
-let i = 0
 
 const letterToHandMap = letter => ({
     A: 1, B: 2, C: 3,
@@ -45,3 +44,21 @@ r01(demo1)
 r01(real1)
 r02(demo1)
 r02(real1)
+
+// BIG BRAIN TIME
+// ray().clearScreen()
+const lines = real1.split("\n");
+ray(
+    lines.map(l => ({
+        "A X": 1 + 3, "A Y": 2 + 6, "A Z": 3 + 0,
+        "B X": 1 + 0, "B Y": 2 + 3, "B Z": 3 + 6,
+        "C X": 1 + 6, "C Y": 2 + 0, "C Z": 3 + 3,
+    }[l])).reduce((sum, i) => sum+i,0)
+)
+ray(
+    lines.map(l => ({
+        "A X": 0 + 3, "A Y": 3 + 1, "A Z": 6 + 2,
+        "B X": 0 + 1, "B Y": 3 + 2, "B Z": 6 + 3,
+        "C X": 0 + 2, "C Y": 3 + 3, "C Z": 6 + 1,
+    }[l])).reduce((sum, i) => sum+i,0)
+)
